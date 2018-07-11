7e27bd6e135f91ef937a88add3db196a76537023
Aaron Stanek <nope>
2018-07-11 16:21:33 -0700

    can now find extrema for double channel binned data

--- extrema.py
+++ extrema.py
@@ -77,3 +77,92 @@ def single_channel(infile,outfile,**args):
     ex["min_x"] = binnum_to_value(ex["min_x_bin"],mm)
     ou = single_channel_format(ex)
     qcsv.to_file(outfile,ou)
+
+def double_channel_confirm_format(indata):
+    if len(indata) < 1:
+        raise format_error
+    if len(indata[0]) != 10:
+        raise format_error
+    if indata[0][0] != "BINS":
+        raise format_error
+    if indata[0][1] != "2":
+        raise format_error
+
+def double_channel_get_mm(indata):
+    ouy = dict()
+    ouy["channel"] = int(indata[0][2])
+    ouy["bins"] = int(indata[0][3])
+    ouy["min"] = float(indata[0][4])
+    ouy["max"] = float(indata[0][5])
+    bins.get_bin_width(ouy,ouy["bins"])
+    oux = dict()
+    oux["channel"] = int(indata[0][6])
+    oux["bins"] = int(indata[0][7])
+    oux["min"] = float(indata[0][8])
+    oux["max"] = float(indata[0][9])
+    bins.get_bin_width(oux,oux["bins"])
+    ou = dict()
+    ou["y"] = ouy
+    ou["x"] = oux
+    return ou
+
+def double_channel_extrema(indata,lims):
+    big_bx = lims["min_x"]
+    big_by = lims["min_y"]
+    big = float(indata[big_by+1][big_bx])
+    small_bx = big_bx
+    small_by = big_by
+    small = big
+    for i in range(lims["min_x"],lims["max_x"]+1):
+        for j in range(lims["min_y"],lims["max_y"]+1):
+            v = float(indata[j+1][i])
+            if v > big:
+                big_bx = i
+                big_by = j
+                big = v
+            if v < small:
+                small_bx = i
+                small_by = j
+                small = v
+    ou = dict()
+    ou["max_x_bin"] = big_bx
+    ou["max_y_bin"] = big_by
+    ou["max_value"] = big
+    ou["min_x_bin"] = small_bx
+    ou["min_y_bin"] = small_by
+    ou["min_value"] = small
+    return ou
+
+def double_channel_format(ex):
+    ou = []
+    ou.append(["MAX"])
+    ou.append(["X",ex["max_x"]])
+    ou.append(["Y",ex["max_y"]])
+    ou.append(["VALUE",ex["max_value"]])
+    ou.append(["MIN"])
+    ou.append(["X",ex["min_x"]])
+    ou.append(["Y",ex["min_y"]])
+    ou.append(["VALUE",ex["min_value"]])
+    return ou
+
+def double_channel(infile,outfile,**args):
+    indata = qcsv.from_file(infile)
+    double_channel_confirm_format(indata)
+    mm = double_channel_get_mm(indata)
+    lims = dict()
+    for p in ["x","y"]:
+        if ("min_"+p) in args:
+            lims["min_"+p] = bins.put_in_bin(args["min_"+p],mm[p],mm[p]["bins"])
+        else:
+            lims["min_"+p] = 0
+        if ("max_"+p) in args:
+            lims["max_"+p] = bins.put_in_bin(args["max_"+p],mm[p],mm[p]["bins"])
+        else:
+            lims["max_"+p] = mm[p]["bins"] - 1
+    ex = double_channel_extrema(indata,lims)
+    ex["max_x"] = binnum_to_value(ex["max_x_bin"],mm["x"])
+    ex["max_y"] = binnum_to_value(ex["max_y_bin"],mm["y"])
+    ex["min_x"] = binnum_to_value(ex["min_x_bin"],mm["x"])
+    ex["min_y"] = binnum_to_value(ex["min_y_bin"],mm["y"])
+    ou = double_channel_format(ex)
+    qcsv.to_file(outfile,ou)

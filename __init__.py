8fa3d0ddb3a574dc5b619ad57cf50fd3e25cc867
Aaron Stanek <amr.stanek@gmail.com>
2018-11-03 16:32:27 -0700

    made it a package

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# in alphabetical order
+
+import background_bins
+import bins
+import calibrate
+import correct_ending_slash
+import cut
+import extrema
+import get_files_in_folder
+import integrate_json_ord_clump
+import integrate_to_csv
+import plot
+import quick_csv
+import read_json_ord_clump
